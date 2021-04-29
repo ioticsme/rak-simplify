9faecd5e031b9a2831f792296c4d8cfa5f3154f0
Ebrahim Nayakkan <ebru.nkn@gmail.com>
2021-04-29 16:37:48 +0400

    first commit

---
+++ src/Simplify.php
@@ -0,0 +1,88 @@
+<?php
+/*
+ * Copyright (c) 2013 - 2020 MasterCard International Incorporated
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are 
+ * permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of 
+ * conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * Neither the name of the MasterCard International Incorporated nor the names of its 
+ * contributors may be used to endorse or promote products derived from this software 
+ * without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE.
+ */
+
+namespace RakBank\Simplify;
+
+use RakBank\Simplify\SimplifyFiles\SimplifyConstants;
+
+// require_once(dirname(__FILE__) . '/SimplifyFiles/Constants.php');
+
+class Simplify
+{
+    /**
+     * @var string $publicKey public API key used to authenticate requests.
+     */
+    public static $publicKey;
+
+    /**
+     * @var string $privateKey private API key used to authenticate requests.
+     */
+    public static $privateKey;
+
+
+    /**
+     * @var string $apiBaseLiveUrl URL of the live API endpoint
+     */
+    public static $apiBaseLiveUrl = SimplifyConstants::API_BASE_LIVE_URL;
+
+    /**
+     * @var string $apiBaseSandboxUrl URL of the sandbox API endpoint
+     */
+    public static $apiBaseSandboxUrl = SimplifyConstants::API_BASE_SANDBOX_URL;
+
+    /**
+     * @var string $userAgent User-agent string send with requests.
+     */
+    public static $userAgent = null;
+
+}
+
+require_once(dirname(__FILE__) . '/SimplifyFiles/SimplifyObject.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/AccessToken.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/SimplifyAuthentication.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/SimplifyPaymentsApi.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Exceptions.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/SimplifyHttp.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/ResourceList.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Authorization.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/CardToken.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Chargeback.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Coupon.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Customer.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Deposit.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Event.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/FraudCheck.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Invoice.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/InvoiceItem.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Tax.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/SimplifyPayment.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Plan.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Refund.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Subscription.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/TransactionReview.php');
+require_once(dirname(__FILE__) . '/SimplifyFiles/Webhook.php');
---
+++ src/SimplifyFiles/Chargeback.php
@@ -0,0 +1,81 @@
+<?php
+/*
+ * Copyright (c) 2013 - 2020 MasterCard International Incorporated
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are 
+ * permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of 
+ * conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * Neither the name of the MasterCard International Incorporated nor the names of its 
+ * contributors may be used to endorse or promote products derived from this software 
+ * without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE.
+ */
+
+use RakBank\Simplify\SimplifyFiles\SimplifyObject;
+use RakBank\Simplify\SimplifyFiles\SimplifyPaymentsApi;
+
+class Simplify_Chargeback extends SimplifyObject {
+
+       /**
+        * Retrieve Simplify_Chargeback objects.
+        * @param     array criteria a map of parameters; valid keys are:<dl style="padding-left:10px;"></dl>
+        * @param     $authentication -  information used for the API call.  If no value is passed the global keys Simplify::public_key and Simplify::private_key are used.  <i>For backwards compatibility the public and private keys may be passed instead of the authentication object.</i>
+        * @return    ResourceList a ResourceList object that holds the list of Chargeback objects and the total
+        *            number of Chargeback objects available for the given criteria.
+        * @see       ResourceList
+        */
+        static public function listChargeback($criteria = null, $authentication = null) {
+
+            $args = func_get_args();
+            
+            $authentication = SimplifyPaymentsApi::buildAuthenticationObject($authentication, $args, 2);
+
+            $val = new Simplify_Chargeback();
+            $list = SimplifyPaymentsApi::listObject($val, $criteria, $authentication);
+
+            return $list;
+        }
+
+
+        /**
+         * Retrieve a Simplify_Chargeback object from the API
+         *
+         * @param     string id  the id of the Chargeback object to retrieve
+         * @param     $authentication -  information used for the API call.  If no value is passed the global keys Simplify::public_key and Simplify::private_key are used.  <i>For backwards compatibility the public and private keys may be passed instead of the authentication object.</i>
+         * @return    Chargeback a Chargeback object
+         */
+        static public function findChargeback($id, $authentication = null) {
+
+            $args = func_get_args();
+            $authentication = SimplifyPaymentsApi::buildAuthenticationObject($authentication, $args, 2);
+
+            $val = new Simplify_Chargeback();
+            $val->id = $id;
+
+            $obj = SimplifyPaymentsApi::findObject($val, $authentication);
+
+            return $obj;
+        }
+
+    /**
+     * @ignore
+     */
+    public function getClazz() {
+        return "Chargeback";
+    }
+}
